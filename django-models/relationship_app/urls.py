01192d4475325c1a6f565ebb46902ee12dc6137d
Rhoda <rhodachela@gmail.com>
2024-11-10 23:20:59 +0300

    User authentication in django

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
-    path('book/add/', add_book, name='add_book'),
+    path('add_book/', add_book, name='add_book'),
     path('book/<int:pk>/edit/', edit_book, name='edit_book'),
     path('book/<int:pk>/delete/', delete_book, name='delete_book'),
 ]
