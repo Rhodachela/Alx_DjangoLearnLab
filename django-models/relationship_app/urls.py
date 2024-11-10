c35aa13e1976efe30f2da5e4771312aefb1a53b7
Rhoda <rhodachela@gmail.com>
2024-11-10 23:14:03 +0300

    User authentication in django

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import list_books
 from .views import CustomLoginView, CustomLogoutView, register
 from django.contrib.auth import views as auth_views
+from .views import admin_view, librarian_view, member_view
+
 
 urlpatterns = [
     path('books/', views.list_books, name = "list_books"),
@@ -10,4 +12,7 @@ urlpatterns = [
     path('login/', CustomLoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', CustomLogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
 ]
