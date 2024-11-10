b1c6c7feae07b0774452adfcbd4ed08196855136
Rhoda <rhodachela@gmail.com>
2024-11-10 21:42:47 +0300

    Advanced model relationships

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -2,8 +2,8 @@ from relationship_app.models import Book, Librarian, Library,Author
 
 def get_book_by_author(author_name):
     try:
-        author = Author.objects.get(name = author_name)
-        books = Author.books.all()
+        author = Author.objects.get(name=author_name)  # Fetch the author by name
+        books = Book.objects.filter(author=author)     # Filter books by this author
         return books
     except Author.DoesNotExist:
         return None
