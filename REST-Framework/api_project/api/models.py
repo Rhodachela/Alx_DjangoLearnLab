8d4304df9b4b2869c11827741872fa6f06d268ec
Rhoda <rhodachela@gmail.com>
2024-11-24 22:11:26 +0300

    Initial commit for api_project with DRF setup

--- REST-Framework/api_project/api/models.py
+++ REST-Framework/api_project/api/models.py
@@ -4,8 +4,8 @@ from django.db import models
 from django.db import models
 
 class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
+    title = models.CharField(max_length= 255)
+    author = models.CharField(max_length= 255)
 
     def __str__(self):
         return self.title
