eeee8fd1b974277dcdea359ec5bbdc2312199885
Rhoda <rhodachela@gmail.com>
2024-11-24 22:48:39 +0300

    Add API endpoint for listing books

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,6 @@ from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()  # Retrieve all book records
     serializer_class = BookSerializer  # Use the BookSerializer
