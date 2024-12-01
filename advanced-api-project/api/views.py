3e4894921b9b699a85abf21d101434fb923897d4
Rhoda <rhodachela@gmail.com>
2024-12-01 21:45:04 +0300

    Custom views and generic views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,8 +2,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # CreateView
 class BookCreateView(generics.CreateAPIView):
