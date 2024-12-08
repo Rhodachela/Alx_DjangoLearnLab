39f296f06a91291c73f41dd90c75c812e76eed17
Rhoda <rhodachela@gmail.com>
2024-12-08 20:28:57 +0300

    Django Blog Application

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -6,12 +6,14 @@ from django.utils.text import slugify
 from taggit.managers import TaggableManager
 from django.forms.widgets import TextInput, Textarea
 from taggit.forms import TagWidget  # Ensure this line is included
+from taggit.forms import TagWidget
+
 
 
 class PostForm(forms.ModelForm):
     tags = TaggableManager(
         help_text="Separate tags with spaces.",
-        # Using TagWidget to enable better UX for tagging
+        # Use TagWidget to make tag entry more user-friendly
         widget=TagWidget(attrs={'class': 'form-control'}),
     )
 
@@ -21,6 +23,7 @@ class PostForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(PostForm, self).__init__(*args, **kwargs)
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
