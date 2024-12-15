0517eb257ac154fec999cac2ce71284d5fce829d
Rhoda <rhodachela@gmail.com>
2024-12-16 00:05:03 +0300

    User follows and feed functionality

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'library_management',
+        'USER': 'your_database_user',  # Replace with your database username
+        'PASSWORD': 'MySQL.installer',  # Replace with your database password
+        'HOST': 'your_database_host',  # e.g., 'localhost' or your database server address
+        'PORT': '3306',  # MySQL default port
     }
-}
+
 
 
 # Password validation
